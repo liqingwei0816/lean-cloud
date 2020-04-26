2008192fe366fcb06979fe80bc2d04babe5f0f01
liqingwei <625368573@qq.com>
2020-04-26 21:15:51 +0800

    spring cloud alibaba 升级到2.2.1 2.2.0有bug 无法自动路由已注册的服务

--- gateway/src/main/java/com/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/gateway/GatewayApplication.java
@@ -3,9 +3,6 @@ package com.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -26,20 +23,24 @@ public class GatewayApplication {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
-    @Bean
+   /* @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
+                .route(p ->
+                    p.path("/**").filters(f->f.stripPrefix(1)).uri("lb://**")
+                ).build();
                 // 配制一个路由,把 http://网关地址:网关端口/demo/ 下的请求路由到 demo-service 微服务中
-                .route(p -> p
+               *//* .route(p -> p
+                       // .path("/provider/**")
                         .path("/provider/**")
                         .filters(f -> f
-                                /* gateway默认不会移除服务名
+                                *//**//* gateway默认不会移除服务名
                                  zuul 中, 例如我们配制的是 把 /demo/** 路由到 http://服务/, 则网关的请求地址: http://网关/demo/xx/abc.do
                                  实际请求的服务地址为: http://服务/xx/abc.do, zuul自动把 /demo 去掉了.
                                     而 Spring Cloud Gateway 不是这样:在Spring Cloud Gateway中,
                                 上面的实际请求的服务地址为: http://服务/demo/xx/abc.do ,
                                 Spring Cloud Gateway不会把 /demo 去掉,与请求的地址一样
-                                 */
+                                 *//**//*
 
                                 .stripPrefix(1)
                                 .hystrix(config -> config   // 对path()指定的请求使用熔断器
@@ -47,7 +48,7 @@ public class GatewayApplication {
                                         .setFallbackUri("forward:/fallback"))
                                         )  // 熔断到 /fallback, 就是上面配制的那个
                         .uri("lb://provider"))  // 将请求路由到指定目标, lb开头是注册中心中的服务
-                .build();
-    }
+                .build();*//*
+    }*/
 
 }
