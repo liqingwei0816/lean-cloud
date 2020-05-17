a6f82522d5563d3b82d079b8039d71ccf1b981a9
liqingwei0816 <a6282560>
2020-05-17 10:52:02 +0800

    日志过滤器优化

--- gateway/src/main/java/com/gateway/config/CachedBodyGlobalFilter.java
+++
@@ -1,9 +0,0 @@
-package com.gateway.config;
-
-import org.springframework.cloud.gateway.filter.AdaptCachedBodyGlobalFilter;
-
-public class CachedBodyGlobalFilter  extends AdaptCachedBodyGlobalFilter{
-
-
-
-}
--- gateway/src/main/java/com/gateway/config/LogFilter.java
+++ gateway/src/main/java/com/gateway/config/LogFilter.java
@@ -31,8 +31,10 @@ import reactor.core.publisher.Mono;
 import java.net.URI;
 import java.nio.charset.Charset;
 import java.util.Objects;
-import java.util.concurrent.atomic.AtomicReference;
 
+/**
+ * 日志打印过滤器
+ */
 @Component
 @Slf4j
 public class LogFilter implements GlobalFilter, Ordered {
@@ -49,79 +51,37 @@ public class LogFilter implements GlobalFilter, Ordered {
         URI requestUri = request.getURI();
         HttpHeaders headers = request.getHeaders();
         MediaType mediaType = headers.getContentType();
+        //日志对象实体
         LogEntity logEntity = new LogEntity();
+        //记录请求类型
         logEntity.setMethod(request.getMethod());
+        //记录请求路径
         logEntity.setUrl(request.getURI().getPath());
         // 排除流文件类型,比如上传的文件contentType.contains("multipart/form-data")
         if (Objects.nonNull(mediaType) && mediaType.getType().contains("multipart/form-data")) {
             logEntity.setRequestBody("上传文件");
-            return chain.filter(exchange);
         }
+        //get方式直接获取请求参数
         if (HttpMethod.GET.equals(request.getMethod())) {
+            //记录请求参数
             logEntity.setRequestBody(requestUri.getQuery());
-            return chain.filter(exchange);
         }
-        if (headers.getContentLength() > 0) {
+        //其他方式，获取请求体
+        if (!HttpMethod.GET.equals(request.getMethod()) && headers.getContentLength() > 0) {
             ServerRequest serverRequest = ServerRequest.create(exchange, HandlerStrategies.withDefaults().messageReaders());
-           return serverRequest.bodyToMono(String.class).flatMap(o->{
-                logEntity.setRequestBody(o);
-                // 重写原始请求
-               ServerHttpRequestDecorator serverHttpRequestDecorator = new ServerHttpRequestDecorator(request) {
-                   @Override
-                   @NonNull
-                   public Flux<DataBuffer> getBody() {
-                       NettyDataBufferFactory nettyDataBufferFactory = new NettyDataBufferFactory(new UnpooledByteBufAllocator(false));
-                       DataBuffer bodyDataBuffer = nettyDataBufferFactory.wrap(logEntity.getRequestBody().getBytes());
-                       return Flux.just(bodyDataBuffer);
-                   }
-               };
-               ServerHttpResponse serverHttpResponse = RewriteUtil.rewriteResponse(exchange, logEntity);
-               return chain.filter(exchange.mutate().request(serverHttpRequestDecorator).response(serverHttpResponse).build());
-           });
-
-
-
-
-
-        } else {
-            return chain.filter(exchange);
+            return serverRequest.bodyToMono(String.class).flatMap(body -> {
+                //记录请求体
+                logEntity.setRequestBody(body);
+                ServerHttpRequest serverHttpRequestDecorator = RewriteUtil.rewriteRequest(request, logEntity);
+                ServerHttpResponse serverHttpResponse = RewriteUtil.rewriteResponse(exchange, logEntity);
+                return chain.filter(exchange.mutate().request(serverHttpRequestDecorator).response(serverHttpResponse).build());
+            });
         }
+        //重写Response对象，并打印日志
+        ServerHttpResponse serverHttpResponse = RewriteUtil.rewriteResponse(exchange, logEntity);
+        return chain.filter(exchange.mutate().response(serverHttpResponse).build());
 
     }
-
-
-
-
-
-  /*  @Override
-    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        try {
-            ServerHttpRequest request = exchange.getRequest();
-            URI requestUri = request.getURI();
-            String schema = requestUri.getScheme();
-
-            // 只记录http、https请求
-            if ((!"http".equals(schema) && !"https".equals(schema))) {
-                return chain.filter(exchange);
-            }
-            //日志对象
-            LogEntity logEntity = new LogEntity();
-            logEntity.setMethod(request.getMethod());
-            logEntity.setUrl(request.getURI().getPath());
-            //请求体只能读一次,获取后重写请求对象
-            ServerHttpRequest serverHttpRequest = RewriteUtil.rewriteRequest(exchange, logEntity);
-            //响应体只能读一次,获取后重写响应对象
-            ServerHttpResponse serverHttpResponse = RewriteUtil.rewriteResponse(exchange, logEntity);
-
-            return chain.filter(exchange.mutate().request(serverHttpRequest).response(serverHttpResponse).build());
-
-        } catch (Exception e) {
-            log.error("请求响应日志打印出现异常", e);
-            return chain.filter(exchange);
-        }
-
-    }
-*/
 
     /**
      * 重写请求对象和响应对象的工具类
@@ -131,38 +91,17 @@ public class LogFilter implements GlobalFilter, Ordered {
         /**
          * 读取请求流并重写请求对象
          */
-        static ServerHttpRequest rewriteRequest(ServerWebExchange exchange, LogEntity logEntity) {
-            ServerHttpRequest request = exchange.getRequest();
-            URI requestUri = request.getURI();
-            HttpHeaders headers = request.getHeaders();
-            MediaType mediaType = headers.getContentType();
-
-            // 排除流文件类型,比如上传的文件contentType.contains("multipart/form-data")
-            if (Objects.nonNull(mediaType) && mediaType.getType().contains("multipart/form-data")) {
-                logEntity.setRequestBody("上传文件");
-                return request;
-            }
-            if (HttpMethod.GET.equals(request.getMethod())) {
-                logEntity.setRequestBody(requestUri.getQuery());
-                return request;
-            }
-            if (headers.getContentLength() > 0) {
-                ServerRequest serverRequest = ServerRequest.create(exchange, HandlerStrategies.withDefaults().messageReaders());
-                AtomicReference<String> reqBody = new AtomicReference<>();
-                logEntity.setRequestBody(reqBody.get());
-                // 重写原始请求
-                return new ServerHttpRequestDecorator(request) {
-                    @Override
-                    @NonNull
-                    public Flux<DataBuffer> getBody() {
-                        NettyDataBufferFactory nettyDataBufferFactory = new NettyDataBufferFactory(new UnpooledByteBufAllocator(false));
-                        DataBuffer bodyDataBuffer = nettyDataBufferFactory.wrap(logEntity.getRequestBody().getBytes());
-                        return Flux.just(bodyDataBuffer);
-                    }
-                };
-            } else {
-                return request;
-            }
+        static ServerHttpRequest rewriteRequest(ServerHttpRequest request, LogEntity logEntity) {
+            // 重写原始请求
+            return new ServerHttpRequestDecorator(request) {
+                @Override
+                @NonNull
+                public Flux<DataBuffer> getBody() {
+                    NettyDataBufferFactory nettyDataBufferFactory = new NettyDataBufferFactory(new UnpooledByteBufAllocator(false));
+                    DataBuffer bodyDataBuffer = nettyDataBufferFactory.wrap(logEntity.getRequestBody().getBytes());
+                    return Flux.just(bodyDataBuffer);
+                }
+            };
         }
 
         /**
@@ -183,7 +122,9 @@ public class LogFilter implements GlobalFilter, Ordered {
                             DataBufferFactory bufferFactory = originalResponse.bufferFactory();
                             DataBuffer join = bufferFactory.join(dataBuffers);
                             String respBody = join.toString(0, join.readableByteCount(), Charset.defaultCharset());
+                            //记录响应数据
                             logEntity.setResponseBody(respBody);
+                            //打印日志
                             log.error(logEntity.toString());
                             return bufferFactory.wrap(respBody.getBytes());
                         }));
@@ -201,8 +142,6 @@ public class LogFilter implements GlobalFilter, Ordered {
     @Getter
     @Setter
     private static class LogEntity {
-        /*log.info("url:{},method:{},请求内容:{},响应内容:{},status:{}",
-       requestUri, method, requestBody.get(), responseBody.get(), serverHttpResponse.getStatusCode());*/
         private String url;
         private HttpMethod method;
         private String requestBody;
@@ -212,8 +151,8 @@ public class LogFilter implements GlobalFilter, Ordered {
         @Override
         public String toString() {
             return "{\n" + url + '\t' + method.name() + "\t" + status.value() +
-                    "\nrequestBody:\n" + requestBody +
-                    "\nresponseBody\n" + responseBody + "}";
+                    "\nreq: " + requestBody +
+                    "\nres " + responseBody + "\n}";
 
         }
     }
